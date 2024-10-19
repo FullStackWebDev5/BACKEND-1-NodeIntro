b8ed4add070c7444d4b6febbb900686d750f26c3
AnkitKumarMishra5 <ankit5kumar14@gmail.com>
2024-10-19 23:39:05 +0530

    Node.js Introduction

--- NodeIntro.js
+++ NodeIntro.js
@@ -1,15 +1,53 @@
 /*
-  # CommonJS Export/Import Syntax
-    - Export
-      module.exports = { sum }
+  # Modules in Node.js
+    - Module: Collection of code, which can be re-used
+    - Benefits:
+      - DRY principle
+      - Maintainability
+      - Code reusability
+      - Code organization
+    
+    - Modules Syntaxes
+      - CommonJS Module (default)
+        - Export
+          const sum = (a, b) => a + b
+          const diff = (a, b) => a - b
+          module.exports = { sum, diff }
 
-    - Import
-      const { sum } = require('./path.js')
+        - Import
+          const { sum, diff } = require('path')
+      - ES6 Module
+        - Export
+          const sum = (a, b) => a + b
+          const diff = (a, b) => a - b
+          export { sum, diff }
+
+        - Import
+          import { sum, diff } from 'path
+
+    - Types of modules
+      - Core/In-built modules: 
+        - Basic functionalities of Node.js
+        - Eg: http (HTTP), fs (FileSystem), etc
+      - User-defined/ Local modules: Done by the developer to follow DRY principle
+      - Third-party modules: External modules, Installed using npm (Node Package Manager)
 
   -----------------------------------------
-  # Built-in modules of Nodejs
-    - http: HTTP
-    - fs: FileSystem
+  # NPM (Node Package Manager)
+    - Package: Collection of re-usable code
+    - NPM: Manage the external dependencies required by the Node.js server
+    - Responsibilities: Install/Update/Uninstall package versions
+    - package.json: Meta data of the node project
+      - scripts: Create script to internally run any other command, should be run using syntax 'npm script-name'
+      - dependencies: List of packages required by the project
+
+    - Commands
+      - npm init: Setup a new Node project
+        - Will create a package.json file
+        - -y flag: Default values
+      - npm install package-name: Install a package
+        - Shorthand command: npm i package-name
+        - For existing projects: 'npm install' will install all packages under dependencies field
 
   -----------------------------------------
   # Node.js boilerplate
@@ -38,4 +76,8 @@
         - path: Path to the file along with filename and extension
         - options:
           - encoding: utf8: UTF8 encoding
+
+    -----------------------------------------
+    # Third-party packages
+      - nodemon: Nodemon - Provides you with a command to run your server and apply changes which saving the file (Live server)
 */
